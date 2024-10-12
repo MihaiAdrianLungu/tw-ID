127467566197a5445f5eb3424ef247f51248dca9
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2024-10-12 11:42:59 +0300

    arrays and objects

---
+++ hoisting.js
@@ -0,0 +1,7 @@
+
+
+console.log(var1);
+
+const var1 = 1;
+
+console.log(var1);
---
+++ obj.js
@@ -0,0 +1,15 @@
+const student = {
+    name: 'Adrian',
+    email: "adrian.lungu@csie.ase.ro",
+    yearsOld: 25,
+    meet: function () {
+        console.log(this.name);
+    },
+    // meet2: () => {
+    //     console.log(this)
+    // }
+}
+
+console.log(student.name);
+console.log(student['name'])
+// console.log(student.meet());
